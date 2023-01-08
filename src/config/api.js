5cbfe2d3916e612b00bd5d1a450acfb3f3751d7d
Alri Diputra <alriydev@gmail.com>
2023-01-08 22:34:19 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const API = axios.create({
-    baseURL: "http://localhost:8000/api/v1/",
+    baseURL: "https://serverdewetour-production.up.railway.app/api/v1/",
 })
 
 export const setAuthToken = (token) => {
